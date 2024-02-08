946253fbc8c0629bb1f515434ec7f9cc3173f6a9
Alexander Fuchs <alex.fu27@gmail.com>
2024-02-08 09:38:28 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+	inputs = {
+			nixpkgs.url = "nixpkgs/nixos-23.11";
+			utils.url = "github:numtide/flake-utils";
+			naersk.url = "github:nix-community/naersk";
+		};
+
+	outputs = { self, nixpkgs, utils, naersk }: 
+		utils.lib.eachDefaultSystem (system: let
+			pkgs = import nixpkgs { inherit system; };
+			naersk = pkgs.callPackage naersk {};
+		in {
+			packages.default = naersk.buildPackage ./daemon ;
+			devShells.default = with pkgs; mkShell {
+				buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+				RUST_SRC_PATH = rustPlatform.rustLibSrc;
+			};
+		});
+}
+
