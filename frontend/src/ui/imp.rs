185b8eb63792ddc11d7e6a1c92b0c493f966e02d
Alexander Fuchs <alex.fu27@gmail.com>
2024-03-23 15:52:07 +0100

    split overview page from app

--- frontend/src/ui/imp.rs
+++ frontend/src/ui/imp.rs
@@ -20,6 +20,7 @@ impl ObjectSubclass for UpdaterWindow {
 	type ParentType = adw::ApplicationWindow;
 
 	fn class_init(klass: &mut Self::Class) {
+		super::UpdaterOverviewPage::ensure_type();
 		klass.bind_template();
 	}
 
@@ -51,3 +52,39 @@ impl WindowImpl for UpdaterWindow {}
 // Trait shared by all application windows
 impl ApplicationWindowImpl for UpdaterWindow {}
 impl AdwApplicationWindowImpl for UpdaterWindow {}
+
+#[derive(CompositeTemplate, Default)]
+#[template(resource = "/de/afuchs/NixOSUpdater/overview.ui")]
+pub struct UpdaterOverviewPage {
+}
+
+// The central trait for subclassing a GObject
+#[glib::object_subclass]
+impl ObjectSubclass for UpdaterOverviewPage {
+	// `NAME` needs to match `class` attribute of template
+	const NAME: &'static str = "UpdaterOverviewPage";
+	type Type = super::UpdaterOverviewPage;
+	type ParentType = adw::Bin;
+
+	fn class_init(klass: &mut Self::Class) {
+		klass.bind_template();
+	}
+
+	fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+		 obj.init_template();
+	}
+}
+
+// Trait shared by all GObjects
+impl ObjectImpl for UpdaterOverviewPage {
+	fn constructed(&self) {
+		// Call "constructed" on parent
+		self.parent_constructed();
+	}
+}
+
+// Trait shared by all widgets
+impl WidgetImpl for UpdaterOverviewPage {}
+
+impl BinImpl for UpdaterOverviewPage {}
+
--- frontend/src/ui/mod.rs
+++ frontend/src/ui/mod.rs
@@ -5,6 +5,24 @@ use gtk::{gio, glib};
 use adw::Application;
 
 glib::wrapper! {
+	 pub struct UpdaterOverviewPage(ObjectSubclass<imp::UpdaterOverviewPage>)
+		  @extends adw::Bin, gtk::Widget,
+		  @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget;
+}
+
+impl Default for UpdaterOverviewPage {
+    fn default() -> Self {
+		  Self::new()
+    }
+}
+
+impl UpdaterOverviewPage {
+	 fn new() -> Self {
+		  Object::builder().build()
+	 }
+}
+
+glib::wrapper! {
 	 pub struct UpdaterWindow(ObjectSubclass<imp::UpdaterWindow>)
 		  @extends adw::ApplicationWindow, gtk::ApplicationWindow, gtk::Window, gtk::Widget,
 		  @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
