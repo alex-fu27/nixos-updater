cf50727cf0b01fed32ba05e6205b59f536e0f58f
Alexander Fuchs <alex.fu27@gmail.com>
2024-12-23 11:47:34 +0100

    flake.nix: update nixpkgs to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
 	inputs = {
-			nixpkgs.url = "nixpkgs/nixos-23.11";
+			nixpkgs.url = "nixpkgs/nixos-24.11";
 			utils.url = "github:numtide/flake-utils";
 			naersk.url = "github:nix-community/naersk";
 			fenix = {
